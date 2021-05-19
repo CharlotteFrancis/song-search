13617a06771f423a3bdad4e00579483d1141b994
Charlotte Wong <ChiharuTaru@gmail.com>
2021-05-19 11:46:10 -0700

    formatting

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 axios.get(`https://api.happi.dev/v1/music?q=${document.getElementById('song_name').value}&limit=&apikey=504db5Pqpbv1duKbtaQ1arufJJtRaPdIyHo7fbhGHMt945gUmpy7KvON&type=&lyrics=1`)
-.then(res => {
-  let songData = res.data
-  console.log(songData)
-  document.getElementById('song_list').innerHTML = ''
-})
-.catch(err => console.error(err))
+  .then(res => {
+    let songData = res.data
+    console.log(songData)
+    document.getElementById('song_list').innerHTML = ''
+  })
+  .catch(err => console.error(err))
